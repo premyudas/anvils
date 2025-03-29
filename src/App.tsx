ae5ea03afa3d0c359eb055423cdc99473ccd9b83
premyudas <90020965+premyudas@users.noreply.github.com>
2025-03-29 12:26:28 -0500

    updated App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
+//import Home from 'src/pages/Home.tsx'
+//import Login from 'src/pages/Login.tsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +16,15 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <p>Uncommon Hacks 2025</p>
-      <p> Anvils</p>
-      <p>Made by: Prem, Catarina, Andrea, Sasha, Surya</p>
+      <p>Uncommon Hacks 2025
+        Anvils
+        Made by: Prem, Catarina, Andrea, Sasha, Surya</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        <button>
+          Home
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
