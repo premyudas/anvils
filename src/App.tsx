125ef1b1d007e67c0315914bbd1a0883fa8e7e53
premyudas <90020965+premyudas@users.noreply.github.com>
2025-03-29 15:50:15 -0500

    organized App and added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ import { Unity, useUnityContext } from 'react-unity-webgl'; // Import Unity WebG
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [home, setHome] = useState(true)
+  const [loggedIn, setLogin] = useState(false);
   const [showUnity, setShowUnity] = useState(false); // state to toggle Unity component
 
   // Configure Unity context with the paths to your Unity build files
@@ -23,28 +24,57 @@ function App() {
 
   return (
     <>
-      <h1>Anvils</h1>
-      <p>Uncommon Hacks 2025 <br/>
-        Made by: Prem, Catarina, Andrea, Sasha, Surya <br/>
-      </p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button>
-          Home
-        </button>
-        <button onClick={() => setShowUnity(!showUnity)}>
-          Show Unity
-        </button>
-        {/* Conditionally render the Unity WebGL content */}
-        {showUnity && (
+      <main>
+        {!home &&
+          <button onClick={() => {
+            setHome(!home)
+            setShowUnity(false)
+          }}>
+            Home
+          </button>
+        }
+        
+        {home &&
+          <div>
             <div>
-
-            <Unity unityProvider={unityProvider} />
+              <h1>Anvils</h1>
+              <p>
+                Uncommon Hacks 2025 <br/>
+                Made by: Prem, Catarina, Andrea, Sasha, Surya <br/>
+              </p>
             </div>
+
+            <div className="card">
+              <button onClick={() => {
+                setLogin(!loggedIn)
+                setHome(!home)
+              }}>
+                Log in/sign up
+              </button>
+
+              <button onClick={() => {
+                setShowUnity(!showUnity)
+                setHome(!home)
+              }}>
+                Continue as Guest
+              </button>
+            </div>
+          </div>
+        }
+
+        {loggedIn && (
+          <div>
+            <p>Taking you to the login page!</p>
+          </div>
         )}
-      </div>
+
+        {showUnity && (
+          <div>
+            <Unity unityProvider={unityProvider} />
+          </div>
+        )}
+
+      </main>
     </>
   )
 }
