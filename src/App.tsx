472941bac29cbaa5664a582cd341a5278d2aedda
Catarina <intrakatx@gmail.com>
2025-03-29 14:55:37 -0500

    app navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
 // import {
 //   createStaticNavigation,
 //   useNavigation,
 // } from '@react-navigation/native';
 //import Home from 'src/pages/Home.tsx'
 // import Login from 'src/pages/Login.tsx'
-
+import { Navigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [goToLogin, setGoToLogin] = React.useState(false);
+  if (goToLogin) {
+    return <Navigate to="src/pages/Login" />;
+  };
 
   return (
     <>
@@ -21,10 +27,10 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button>
+        <button onClick={() => setGoToLogin(true)}>
           Login
         </button>
-        <button>
+        <button> 
           Continue as Guest
         </button>
       </div>
